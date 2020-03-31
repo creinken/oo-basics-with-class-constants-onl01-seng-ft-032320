3150ea2a21f922256d5741f7fbf4bce50d6af142
Christopher Reinken <christopherreinken@gmail.com>
2020-03-31 13:56:14 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,7 @@ class Shoe
   
   def initialize(brand)
     @brand = brand
-    if BRANDS.any?{|word| word = @brand}
+    if BRANDS.any?{|word| word == @brand}
       
     else
       BRANDS << @brand
